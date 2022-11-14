ebe7935744b5f20a60034b9a649fa022eab9ff4d
Gibadullina <gibadullina.elina@inbox.ru>
2022-11-14 22:26:00 +0500

    Добавлены индикаторы, затраты, прибыль, доход

--- js/butt.js
+++ js/butt.js
@@ -2,22 +2,41 @@
    var ff=true;
    var checkStart=false;
 
+   var soldEggs=0;
+
+   var soldSum=0;
+
+   
       $(document).ready(function() {
-	   $(".contr").hide();
+	   $('#imit').hide();
+	  
+	   document.getElementById("soldT").innerText="Продано единиц продукции: "+soldEggs+" шт";
+	   document.getElementById("sumT").innerText="Сумма продаж: "+soldSum+" руб.";
 	   $("#finish").hide();
 	   $("a.fancy").fancyZoom();
 	  // начало имитации
 	  $("#start").click(function() {
 	   $(this).hide();
+	   
+	    //показываем кнопки управления и выхода
 	   $("#finish").show();
-	    $(".contr").show();
+	     //показываем инкубатор
+	   $('#imit').show();
+	 
+
 	   checkStart=true;
 	  
 	   //продать яйца
 	   $("#sell").click(function() {
+		   if (sold==false) {
 	 $("#ink").attr("src", "../img/5.png");
 	 $("#ink1").attr("href", "../img/4.png");
+	 soldEggs+=10;
+	 soldSum=soldSum+soldEggs*50;
+	 document.getElementById("soldT").innerText="Продано единиц продукции: "+soldEggs+" шт";
+	 document.getElementById("sumT").innerText="Сумма продаж: "+soldSum+" руб.";
 	   sold=true;
+		   }
          })
 		 
        //переворот
@@ -38,7 +57,25 @@
 	   	  $("#finish").click(function() {
 	   $(this).hide();
 	   $("#start").show();
-	    $(".contr").hide();
+	   //скрываем элементы имитациии
+	    $('#imit').hide();
 	   })
 	    
+		
+		//Индикаторы
+		function idfic() {
+		var tem= [37,38,39];
+		var humid=[];
+		 for (var i=0;i<12;i++) {
+			 humid[i]=50+i;
+		 }
+		var tt=Math.random();
+		//температура
+		document.getElementById("temp").innerText="Температура: "+parseFloat(tem[Math.floor(Math.random() * tem.length)]+parseFloat(tt.toFixed(2)))+" C";
+		//влажность
+		document.getElementById("hum").innerText="Влажность: "+humid[Math.floor(Math.random() * humid.length)]+" %";
+		var t = setTimeout(function(){ idfic() }, 1000); 
+		}
+		idfic();
+
    });
--- js/test.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
- $("#sell").click(function() {
-	 $("#ink").attr("href", "../img/4.png");
- })
-})
